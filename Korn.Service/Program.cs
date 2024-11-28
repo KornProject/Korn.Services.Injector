9dcd6f67851e9e977d8e21916b472b938fd1d319
Yotic <yoticcitoy@gmail.com>
2024-11-28 21:09:36 +0300

    Added WqlEventQuery for tests. Works fine and blazingly fast and takes 8-10ms to find a process

--- Korn.Service/Program.cs
+++ Korn.Service/Program.cs
@@ -1,1 +1,31 @@
-﻿File.WriteAllText(@"C:\a.txt", "test");
+﻿using Korn.Utils.System;
+using System.Diagnostics;
+using System.Management;
+
+var startWatch = new ManagementEventWatcher(
+      new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace"));
+startWatch.EventArrived += startWatch_EventArrived;
+startWatch.Start();
+
+Thread.Sleep(TimeSpan.MaxValue);
+
+void startWatch_EventArrived(object sender, EventArrivedEventArgs e)
+{
+    var properties = e.NewEvent.Properties;
+    var name = (string)properties["ProcessName"].Value;
+    var id = (int)(uint)properties["ProcessID"].Value;
+
+    var a = DateTime.Now.Ticks;
+    Console.WriteLine("Process started: {0}", name);
+    if (name == "ConsoleTest.exe")
+    {
+        var process = Process.GetProcessById(id);
+        using var processManager = new ExternalProcessManager(process);
+
+        processManager.SuspendProcess();
+
+        _ = 3;
+    }
+
+    _ = 3;
+}
